81e2d81163ac088e1a9fdb36c8c56bd2f3d5722a
Kees Bakker <kees@sodaq.net>
2013-10-21 19:42:48 +0200

    Add LGPL license in source

--- GPRSbee.h
+++ GPRSbee.h
@@ -1,12 +1,24 @@
-/*
- * GPRSbee.h
- *
- *  Created on: Oct 19, 2013
- *      Author: Kees Bakker
- */
-
 #ifndef GPRSBEE_H_
 #define GPRSBEE_H_
+/*
+ * Copyright (c) 2013 Kees Bakker.  All rights reserved.
+ *
+ * This file is part of GPRSbee.
+ *
+ * GPRSbee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published bythe Free Software Foundation, either version 3 of
+ * the License, or(at your option) any later version.
+ *
+ * GPRSbee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GPRSbee.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdint.h>
 #include <Arduino.h>
