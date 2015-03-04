ae077eb07b71b8a87e8300d2d50d5f20dca27b66
Kees Bakker <kees@m2m4all.com>
2015-03-04 21:28:43 +0100

    Add new class SIMDateTime to be used together with the AT+CCLK

--- GPRSbee.cpp
+++ GPRSbee.cpp
@@ -21,6 +21,7 @@
 #include <Arduino.h>
 #include <Stream.h>
 #include <avr/wdt.h>
+#include <stdlib.h>
 
 #include "GPRSbee.h"
 
@@ -1973,4 +1974,198 @@ void GPRSbeeClass::disableCIURC()
 {
   if (!sendCommandWaitForOK_P(PSTR("AT+CIURC=0"), 6000)) {
   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+SIMDateTime::SIMDateTime(uint8_t y, uint8_t m, uint8_t d, uint8_t hh, uint8_t mm, uint8_t ss, int8_t tz)
+{
+  _yOff = y;
+  _m = m;
+  _d = d;
+  _hh = hh;
+  _mm = mm;
+  _ss = ss;
+  _tz = tz;
+}
+
+/*
+ * \brief Construct from a timestamp (seconds since Y2K Epoch)
+ */
+SIMDateTime::SIMDateTime(uint32_t ts)
+{
+  // whole and fractional parts of 1 day
+  uint16_t days = ts / 86400UL;
+  int32_t fract = ts % 86400UL;
+  //Serial.print(F("days ")); Serial.println(days);
+
+  // Extract hour, minute, and second from the fractional day
+  ldiv_t lresult = ldiv(fract, 60L);
+  _ss = lresult.rem;
+  div_t result = div(lresult.quot, 60);
+  _mm = result.rem;
+  _hh = result.quot;
+
+  //
+  uint16_t n = days + SATURDAY;
+  n %= 7;
+  //Serial.print(F("wday ")); Serial.println(n);
+  // _wday = n;
+
+  // map into a 100 year cycle
+  lresult = ldiv((long) days, 36525L);
+  uint16_t years = 100 * lresult.quot;
+  //Serial.print(F("years ")); Serial.println(years);
+
+  // map into a 4 year cycle
+  lresult = ldiv(lresult.rem, 1461L);
+  years += 4 * lresult.quot;
+  days = lresult.rem;
+  if (years > 100) {
+    ++days;
+  }
+
+  // 'years' is now at the first year of a 4 year leap cycle, which will always be a leap year,
+  // unless it is 100. 'days' is now an index into that cycle.
+  uint8_t leapyear = 1;
+  if (years == 100) {
+    leapyear = 0;
+  }
+
+  // compute length, in days, of first year of this cycle
+  n = 364 + leapyear;
+
+  /*
+   * if the number of days remaining is greater than the length of the
+   * first year, we make one more division.
+   */
+  if (days > n) {
+      days -= leapyear;
+      leapyear = 0;
+      result = div(days, 365);
+      years += result.quot;
+      days = result.rem;
+  }
+  //Serial.print(F("years ")); Serial.println(years);
+  _yOff = years;
+  //Serial.print(F("days ")); Serial.println(days);
+  // _ydays = days;
+
+  /*
+   * Given the year, day of year, and leap year indicator, we can break down the
+   * month and day of month. If the day of year is less than 59 (or 60 if a leap year), then
+   * we handle the Jan/Feb month pair as an exception.
+   */
+  n = 59 + leapyear;
+  if (days < n) {
+      /* special case: Jan/Feb month pair */
+      result = div(days, 31);
+      _m = result.quot;
+      _d = result.rem;
+  } else {
+      /*
+       * The remaining 10 months form a regular pattern of 31 day months alternating with 30 day
+       * months, with a 'phase change' between July and August (153 days after March 1).
+       * We proceed by mapping our position into either March-July or August-December.
+       */
+      days -= n;
+      result = div(days, 153);
+      _m = 2 + result.quot * 5;
+
+      /* map into a 61 day pair of months */
+      result = div(result.rem, 61);
+      _m += result.quot * 2;
+
+      /* map into a month */
+      result = div(result.rem, 31);
+      _m += result.quot;
+      _d = result.rem;
+  }
+
+  _tz = 0;
+}
+
+/*
+ * \brief Construct using a text string as received from AT+CCLK
+ *
+ * format is "yy/MM/dd,hh:mm:ss±zz"
+ *
+ * No serious attempt is made to validate the string. Whatever comes
+ * in is used as is. Each number is assumed to have two digits.
+ *
+ * Also, you year number is assumed to be the offset of 2000
+ *
+ * Example input string: 04/01/02,00:47:32+04
+ */
+SIMDateTime::SIMDateTime(const char * cclk)
+{
+  _yOff = conv2d(cclk);
+  cclk += 3;
+  _m = conv2d(cclk) - 1;                // Month is 0 based
+  cclk += 3;
+  _d = conv2d(cclk) - 1;                // Day is 0 based
+  cclk += 3;
+  _hh = conv2d(cclk);
+  cclk += 3;
+  _mm = conv2d(cclk);
+  cclk += 3;
+  _ss = conv2d(cclk);
+  cclk += 2;
+  uint8_t isNeg = *cclk == '-';
+  ++cclk;
+  _tz = conv2d(cclk);
+  if (isNeg) {
+    _tz = -_tz;
+  }
+}
+
+static const uint8_t daysInMonth [] PROGMEM = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+/*
+ * \brief Compute the Y2K Epoch from the date and time
+ */
+uint32_t SIMDateTime::getY2KEpoch() const
+{
+  uint32_t ts;
+  uint16_t days = _d + (365 * _yOff) + ((_yOff + 3) / 4);
+  // Add the days of the previous months in this year.
+  for (uint8_t i = 0; i < _m; ++i) {
+    days += pgm_read_byte(daysInMonth + i);
+  }
+  if ((_m > 2) && ((_yOff % 4) == 0)) {
+    ++days;
+  }
+
+  ts = ((uint32_t)days * 24) + _hh;
+  ts = (ts * 60) + _mm;
+  ts = (ts * 60) + _ss;
+
+  ts = ts - (_tz * 15 * 60);
+
+  return ts;
+}
+
+/*
+ * \brief Compute the UNIX Epoch from the date and time
+ */
+uint32_t SIMDateTime::getUnixEpoch() const
+{
+  return getY2KEpoch() + 946684800;
+}
+
+uint8_t SIMDateTime::conv1d(const char * txt)
+{
+  uint8_t       val = 0;
+  if (*txt >= '0' && *txt <= '9') {
+    val = *txt - '0';
+  }
+  return val;
+}
+
+uint8_t SIMDateTime::conv2d(const char * txt)
+{
+  uint8_t val = conv1d(txt++) * 10;
+  val += conv1d(txt);
+  return val;
 }
--- GPRSbee.h
+++ GPRSbee.h
@@ -28,6 +28,51 @@
 // diagnostic
 #define ENABLE_GPRSBEE_DIAG     1
 
+/*
+ * \brief A class to store clock values
+ */
+class SIMDateTime
+{
+public:
+  SIMDateTime(uint32_t ts=0);
+  SIMDateTime(uint8_t y, uint8_t m, uint8_t d, uint8_t hh, uint8_t mm, uint8_t ss, int8_t tz=0);
+  SIMDateTime(const char * cclk);
+
+  enum _WEEK_DAYS_ {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+  };
+
+  uint16_t      year() { return _yOff + 2000; }
+  uint8_t       month() { return _m + 1; }
+  uint8_t       day() { return _d + 1; }
+  uint8_t       hour() { return _hh; }
+  uint8_t       minute() { return _mm; }
+  uint8_t       second() { return _ss; }
+
+  // 32-bit number of seconds since Unix epoch (1970-01-01)
+  uint32_t getUnixEpoch() const;
+  // 32-bit number of seconds since Y2K epoch (2000-01-01)
+  uint32_t getY2KEpoch() const;
+
+private:
+  uint8_t       conv1d(const char * txt);
+  uint8_t       conv2d(const char * txt);
+
+  uint8_t       _yOff;          // Year value minus 2000
+  uint8_t       _m;             // month (0..11)
+  uint8_t       _d;             // day (0..30)
+  uint8_t       _hh;            // hour (0..23)
+  uint8_t       _mm;            // minute (0..59)
+  uint8_t       _ss;            // second (0..59)
+  int8_t        _tz;            // timezone (multiple of 15 minutes)
+};
+
 class GPRSbeeClass
 {
 public:
